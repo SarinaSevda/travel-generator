2e0d2e9ad299c42bff25a03b67d8b5fed7685cfb
SarinaSevda <sarina.schoki@gmail.com>
2024-02-24 18:34:46 +0100

    Improved the user experience

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function displayTrip(response) {
-  console.log("trip generated");
   new Typewriter("#ai-answer", {
     strings: response.data.answer,
     autoStart: true,
@@ -11,8 +10,6 @@ function displayTrip(response) {
 function generateTrip(event) {
   event.preventDefault();
 
-  //let aiAnswerElement = document.querySelector("#ai-answer");
-
   let userInput = document.querySelector("#user-input");
   let apiKey = "4569530f182bfa0dfo5t468f004ad4c8";
   let prompt = `User input: Generate a trip for ${userInput.value}`;
@@ -20,9 +17,9 @@ function generateTrip(event) {
     "You are a travel expert who has travelled the whole world and knows the best spots and highlights of each city and country in the world. Your mission is to generate a 10 line trip suggestion. Make sure to follow the user input.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("generating trip");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let answerElement = document.querySelector("#ai-answer");
+  answerElement.classList.remove("hidden");
+  answerElement.innerHTML = `<div class="generating">⏳ Generating a trip to  ${userInput.value}</div>`;
 
   axios.get(apiURL).then(displayTrip);
 }
